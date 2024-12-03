4c80dda7fa06f7df75e55b9f7c62b154e5d697df
Viktoria <v.s.shuvaeva@gmail.com>
2024-12-03 20:42:26 +0700

    fix

--- app/routes/account_route.py
+++ app/routes/account_route.py
@@ -1,9 +1,7 @@
 from flask import Blueprint, jsonify, request, render_template
 from flask_jwt_extended import jwt_required, get_jwt_identity
 import logging
-import json
-# Получаем логгер по его имени
-logger = logging.getLogger('chatbot')
+
 
 account_bp = Blueprint('account', __name__)
 
@@ -24,10 +22,10 @@ def account():
 def protected():
     try:
         current_user = get_jwt_identity()
-        logger.info(f"Пользователь авторизован: {current_user}")
+        logging.info(f"Пользователь авторизован: {current_user}")
         return jsonify({"message": "Access granted"}), 200
     except Exception as e:
-        logger.error(f"Ошибка авторизации: {str(e)}")
+        logging.error(f"Ошибка авторизации: {str(e)}")
         return jsonify({"error": "Authorization failed"}), 401
 
 
@@ -35,13 +33,12 @@ def protected():
 @jwt_required()  # Требуется авторизация с JWT
 def get_username():
     current_user = get_jwt_identity()
-    current_user=json.loads(current_user)
     from app.database.managers.user_manager import UserManager
     # Создаем экземпляр менеджера базы данных
     db = UserManager()
-    logger.info(f"Запрос имени пользователя от пользователя: {current_user['user_id']}", extra={'user_id': current_user['login']})
-    user=db.get_user_by_user_id(current_user['user_id'])
+    logging.info(f"Запрос имени пользователя от пользователя: {current_user}")
+    user=db.get_user_by_user_id(current_user)
     username=user.username
-    logger.info(f"Получено имя пользователя: {username}", extra={'user_id': current_user['login']})
+    logging.info(f"Получено имя пользователя: {username}")
     return jsonify(username), 200
 
