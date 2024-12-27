82ac8287f1f988b55818c51defb37ef6390b865b
Viktoria <v.s.shuvaeva@gmail.com>
2024-12-27 15:17:37 +0700

    fix

--- config/config_celery.py
+++ config/config_celery.py
@@ -7,5 +7,5 @@ load_dotenv()
 class ConfigCelery:
     redis_port = os.getenv('REDIS_PORT')
     # Используем Redis для Celery
-    CELERY_BROKER_URL = f'redis://localhost:{redis_port}/0'
-    CELERY_RESULT_BACKEND = f'redis://localhost:{redis_port}/0'
+    CELERY_BROKER_URL = 'redis://redis:6379/0'
+    CELERY_RESULT_BACKEND = 'redis://redis:6379/0'
