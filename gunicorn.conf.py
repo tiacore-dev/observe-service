bc18dab57d195df2461a22b1df5f8628a3e15ef5
Viktoria <v.s.shuvaeva@gmail.com>
2024-12-29 19:43:22 +0700

    eventlet

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,3 +1,4 @@
+import multiprocessing
 import os
 from dotenv import load_dotenv
 
@@ -6,8 +7,16 @@ load_dotenv()
 port = os.getenv('FLASK_PORT')
 
 bind = f"0.0.0.0:{port}"
-workers = 4
-timeout = 600
+
+# Получаем количество доступных ядер CPU
+cpu_count = multiprocessing.cpu_count()
+
+workers = cpu_count * 2  # Количество воркеров: 2 воркера на ядро
+threads = 2  # Количество потоков на воркер
+worker_class = "eventlet"  # Используем eventlet для асинхронной обработки
+worker_connections = 1000  # Максимум соединений на воркера
+timeout = 120  # Таймаут запросов
+
 
 # Логи
 loglevel = "info"
