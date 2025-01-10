a80386315176e5dffec4dc1e452bf6dcd92a5735
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-11 00:39:44 +0700

    fix logging s3_manager

--- app/s3/s3_manager.py
+++ app/s3/s3_manager.py
@@ -24,35 +24,35 @@ class S3Manager:
         :param bucket_name: Название S3-бакета.
         :param object_name: Имя объекта в S3.
         """
-        logging.info(f"Загрузка файла в bucket '{
-                     bucket_name}' с именем 'telegram_docs/{object_name}'.")
+        logging.info(f"""Загрузка файла в bucket '{
+                     bucket_name}' с именем 'telegram_docs/{object_name}'.""")
         try:
             # Используем upload_fileobj для загрузки данных из памяти
             self.s3.upload_fileobj(
                 file_content, bucket_name, f"telegram_docs/{object_name}")
-            logging.info(f"Файл успешно загружен в {
-                         bucket_name}/telegram_docs/{object_name}.")
+            logging.info(f"""Файл успешно загружен в {
+                         bucket_name}/telegram_docs/{object_name}.""")
         except ClientError as e:
             logging.error(f"Ошибка при загрузке файла: {e}")
             raise
 
     def upload_fileobj(self, file_obj, bucket_name, object_name):
         """Загружает файл напрямую из объекта файла в S3."""
-        logging.info(f"Загрузка файла в bucket '{
-                     bucket_name}' с именем '{object_name}'.")
+        logging.info(f"""Загрузка файла в bucket '{
+                     bucket_name}' с именем '{object_name}'.""")
         try:
             self.s3.upload_fileobj(file_obj, bucket_name,
                                    "telegram_docs/"+object_name)
-            logging.info(f"Файл успешно загружен в {
-                         bucket_name}/{object_name}.")
+            logging.info(f"""Файл успешно загружен в {
+                         bucket_name}/{object_name}.""")
         except ClientError as e:
             logging.error(f"Ошибка при загрузке файла: {e}")
 
     def download_file(self, bucket_name, object_name, file_name):
         if file_name is None:
             file_name = object_name
-        logging.info(f"Скачивание файла '{object_name}' из bucket '{
-                     bucket_name}' в '{file_name}'.")
+        logging.info(f"""Скачивание файла '{object_name}' из bucket '{
+                     bucket_name}' в '{file_name}'.""")
         try:
             self.s3.download_file(bucket_name, object_name,
                                   "telegram_docs/"+file_name)
@@ -62,8 +62,8 @@ class S3Manager:
             logging.error(f"Ошибка при скачивании файла '{object_name}': {e}")
 
     def list_files(self, bucket_name, prefix=""):
-        logging.info(f"Получение списка файлов из bucket '{
-                     bucket_name}' с префиксом '{prefix}'.")
+        logging.info(f"""Получение списка файлов из bucket '{
+                     bucket_name}' с префиксом '{prefix}'.""")
         try:
             response = self.s3.list_objects_v2(
                 Bucket=bucket_name, Prefix=prefix)
@@ -71,38 +71,38 @@ class S3Manager:
             logging.info(f"Найдено {len(files)} файлов.")
             return files
         except ClientError as e:
-            logging.error(f"Ошибка при получении списка файлов из bucket '{
-                          bucket_name}': {e}")
+            logging.error(f"""Ошибка при получении списка файлов из bucket '{
+                          bucket_name}': {e}""")
             return []
 
     def delete_file(self, bucket_name, object_name):
-        logging.info(f"Удаление файла '{
-                     object_name}' из bucket '{bucket_name}'.")
+        logging.info(f"""Удаление файла '{
+                     object_name}' из bucket '{bucket_name}'.""")
         try:
             self.s3.delete_object(Bucket=bucket_name,
                                   Key="telegram_docs/"+object_name)
-            logging.info(f"Файл '{object_name}' успешно удален из bucket '{
-                         bucket_name}'.")
+            logging.info(f"""Файл '{object_name}' успешно удален из bucket '{
+                         bucket_name}'.""")
         except ClientError as e:
             logging.error(f"Ошибка при удалении файла '{object_name}': {e}")
 
     def file_exists(self, bucket_name, object_name):
-        logging.info(f"Проверка существования файла '{
-                     object_name}' в bucket '{bucket_name}'.")
+        logging.info(f"""Проверка существования файла '{
+                     object_name}' в bucket '{bucket_name}'.""")
         try:
             self.s3.head_object(Bucket=bucket_name,
                                 Key="telegram_docs/"+object_name)
-            logging.info(f"Файл '{object_name}' существует в bucket '{
-                         bucket_name}'.")
+            logging.info(f"""Файл '{object_name}' существует в bucket '{
+                         bucket_name}'.""")
             return True
         except ClientError as e:
-            logging.warning(f"Файл '{object_name}' не найден в bucket '{
-                            bucket_name}': {e}")
+            logging.warning(f"""Файл '{object_name}' не найден в bucket '{
+                            bucket_name}': {e}""")
             return False
 
     def get_file_metadata(self, bucket_name, object_name):
-        logging.info(f"Получение метаданных для файла '{
-                     object_name}' из bucket '{bucket_name}'.")
+        logging.info(f"""Получение метаданных для файла '{
+                     object_name}' из bucket '{bucket_name}'.""")
         try:
             response = self.s3.head_object(
                 Bucket=bucket_name, Key="telegram_docs/"+object_name)
@@ -113,13 +113,13 @@ class S3Manager:
                 'LastModified': response['LastModified'],
             }
         except ClientError as e:
-            logging.error(f"Ошибка при получении метаданных для файла '{
-                          object_name}': {e}")
+            logging.error(f"""Ошибка при получении метаданных для файла '{
+                          object_name}': {e}""")
             return None
 
     def get_file(self, bucket_name, object_name):
-        logging.info(f"Получение файла '{
-                     object_name}' из bucket '{bucket_name}'.")
+        logging.info(f"""Получение файла '{
+                     object_name}' из bucket '{bucket_name}'.""")
         try:
             response = self.s3.get_object(
                 Bucket=bucket_name, Key="telegram_docs/"+object_name)
@@ -132,17 +132,17 @@ class S3Manager:
             return None
 
     def generate_presigned_url(self, bucket_name, object_name, expiration=3600):
-        logging.info(f"Генерация временного URL для файла '{object_name}' в bucket '{
-                     bucket_name}', срок действия {expiration} секунд.", extra={'user_id': 's3'})
+        logging.info(f"""Генерация временного URL для файла '{object_name}' в bucket '{
+                     bucket_name}', срок действия {expiration} секунд.""", extra={'user_id': 's3'})
         try:
             response = self.s3.generate_presigned_url('get_object',
                                                       Params={
                                                           'Bucket': bucket_name, 'Key': object_name},
                                                       ExpiresIn=expiration)
-            logging.info(f"URL успешно сгенерирован для файла '{
-                         object_name}'.", extra={'user_id': 's3'})
+            logging.info(f"""URL успешно сгенерирован для файла '{
+                         object_name}'.""", extra={'user_id': 's3'})
             return response
         except ClientError as e:
-            logging.error(f"Ошибка при генерации URL для файла '{
-                          object_name}': {e}", extra={'user_id': 's3'})
+            logging.error(f"""Ошибка при генерации URL для файла '{
+                          object_name}': {e}""", extra={'user_id': 's3'})
             return None
