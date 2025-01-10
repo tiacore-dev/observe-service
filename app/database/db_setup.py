bb64ea5e629d6ab970782ce940edbd733f8da739
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-10 15:43:11 +0700

    fix

--- app/database/db_setup.py
+++ app/database/db_setup.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy.ext.declarative import declarative_base
-from db_globals import Session, Base, engine
+from app.database.db_globals import Session, Base, engine
 
 if not Base:
     Base = declarative_base()
