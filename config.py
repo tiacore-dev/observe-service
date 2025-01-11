e6f42cb3564d75625f1ceb461b467aacdec14712
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-11 19:00:03 +0700

    config broker url and numpy

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ class ProductionConfig(ConfigFlask):
 
 
 class CeleryConfig(ConfigFlask):
-    CELERY_BROKER_URL = 'redis://redis:6379/0'
+    broker_url = 'redis://redis:6379/0'
     result_backend = 'redis://redis:6379/0'
     # Таймаут соединения с брокером (в секундах)
     broker_transport_options = {
