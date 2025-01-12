e7385eb0124e62e645009bc79cdbd723b0b93b6d
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-12 23:19:09 +0700

    eventlet

--- wsgi_gateway.py
+++ wsgi_gateway.py
@@ -1,5 +1,11 @@
-import os
+# autopep8: off
+import eventlet
+eventlet.monkey_patch()
+
+
 from app import create_app
+import os
+# autopep8: on
 
 
 config_name = os.environ.get('CONFIG_NAME', 'Development')
--- wsgi_scheduler.py
+++ wsgi_scheduler.py
@@ -1,6 +1,11 @@
-import os
-from app import create_app
+# autopep8: off
+import eventlet
+eventlet.monkey_patch()
 
+
+from app import create_app
+import os
+# autopep8: on
 
 config_name = os.environ.get('CONFIG_NAME', 'Development')
 app = create_app(config_name, enable_scheduler=True)
