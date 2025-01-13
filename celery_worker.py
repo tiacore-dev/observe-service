39969c82c8351f9ac8223b05d586900452518493
Viktoria <v.s.shuvaeva@gmail.com>
2025-01-13 10:31:07 +0700

    fix

--- celery_worker.py
+++ celery_worker.py
@@ -1,5 +1,11 @@
+# autopep8: off
+from gevent import monkey # pylint: disable=import-error
+monkey.patch_all()
+
 from app import create_app
 from app_celery.celery_app import create_celery_app
+# autopep8: on
+
 
 print('RUNNING CELERY WORKER')
 
--- wsgi_gateway.py
+++ wsgi_gateway.py
@@ -1,10 +1,5 @@
-# autopep8: off
-from gevent import monkey # pylint: disable=import-error
-monkey.patch_all()
-
 from app import create_app
 import os
-# autopep8: on
 
 
 config_name = os.environ.get('CONFIG_NAME', 'Development')
