7ad0d8a232e37fa912d7f864a87647bccc656928
Viktoria <v.s.shuvaeva@gmail.com>
2025-02-17 12:35:14 +0700

    fix

--- app/routes/api_route.py
+++ app/routes/api_route.py
@@ -56,6 +56,25 @@ def get_messages():
         logging.error(f"Ошибка при фильтрации сообщений: {str(e)}")
         return jsonify({'error': 'Failed to fetch messages'}), 500
 
+# Функция для безопасного парсинга даты
+
+
+def parse_datetime(date_str, default_time):
+    if not date_str:
+        return None
+    try:
+        # Формат с временем
+        return datetime.strptime(date_str, "%Y-%m-%d %H:%M:%S")
+    except ValueError:
+        try:
+            return datetime.strptime(date_str, "%Y-%m-%d").replace(
+                hour=default_time[0], minute=default_time[1], second=default_time[2]
+            )  # Если время отсутствует
+        except ValueError as e:
+            logging.error(
+                f"Ошибка при разборе даты: {date_str} - {str(e)}")
+            return None
+
 
 @api_bp.route('/api/analyze', methods=['GET'])
 @jwt_required()
@@ -74,17 +93,14 @@ def get_all_messages_for_analysis():
     from app.database.managers.message_manager import MessageManager
     manager = MessageManager()
 
-    # Преобразуем start_date и end_date, добавляя время, если нужно
-    if start_date and isinstance(start_date, str):
-        start_date = datetime.strptime(start_date, "%Y-%m-%d")
-        start_date = start_date.replace(hour=0, minute=0, second=0)
-    if end_date and isinstance(end_date, str):
-        end_date = datetime.strptime(end_date, "%Y-%m-%d")
-        end_date = end_date.replace(hour=23, minute=59, second=59)
+    # Корректируем start_date и end_date
+    start_date = parse_datetime(start_date, (0, 0, 0))  # Начало дня
+    end_date = parse_datetime(end_date, (23, 59, 59))  # Конец дня
 
     try:
         messages = manager.get_filtered_messages(
-            start_date=start_date, end_date=end_date, user_id=user_id, chat_id=chat_id)
+            start_date=start_date, end_date=end_date, user_id=user_id, chat_id=chat_id
+        )
         logging.info(f"Найдено {len(messages)} сообщений для анализа.")
         return jsonify({'messages': [msg.to_dict() for msg in messages]}), 200
     except Exception as e:
